110f292bcf25378412edbc4878f20838de879ac3
TranHaiBang12 <133078395+TranHaiBang12@users.noreply.github.com>
2023-06-06 22:20:11 +0700

    .

--- Cinema/src/java/controller/LoginServlet.java
+++ Cinema/src/java/controller/LoginServlet.java
@@ -8,10 +8,12 @@ import dal.AccountDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.jsp.PageContext;
 import model.Account;
 
 /**
@@ -32,6 +34,21 @@ public class LoginServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        Cookie[] arr = request.getCookies();
+        String user = "";
+        String pass = "";
+        String checked = "";
+        for (Cookie i : arr) {
+            if (i.getName().equals("user")) {
+                user = i.getValue();
+            }
+            else if (i.getName().equals("pass")) {
+                pass = i.getValue();
+            }
+            else if (i.getName().equals("rem")) {
+                checked = i.getValue();
+            }
+        }
         try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
@@ -70,6 +87,11 @@ public class LoginServlet extends HttpServlet {
                     + "                font-weight: bold;\n"
                     + "                text-decoration: none;\n"
                     + "                color: white;\n"
+                    + "            }\n"
+                    + "            #rmbMe{\n"
+                    + "                width: 15px;\n"
+                    + "                height: 15px;\n"
+                    + "                margin-right: 15px;\n"
                     + "            }\n"
                     + "\n"
                     + "            .dn {\n"
@@ -129,7 +151,6 @@ public class LoginServlet extends HttpServlet {
                     + "                padding-bottom: 30px;\n"
                     + "            }\n"
                     + "            h2 {\n"
-                    
                     + "                margin-left:20px;\n"
                     + "                margin-top:20px;\n"
                     + "                color:red\n"
@@ -228,30 +249,34 @@ public class LoginServlet extends HttpServlet {
                     + "                <li><a href=\"#\"><img class=\"storeLogo\" src=\"images/storeiCON.png\" /></a></li>\n"
                     + "            </ul>\n"
                     + "\n"
-                    + "\n" +
-            "        </div>"
+                    + "\n"
+                    + "        </div>"
                     + "        <div class = \"body\">\n"
                     + "            <div class = \"logn\">\n"
                     + "                <div class = \"loginTitle\">\n"
                     + "                    <a href = \"login\" class = \"dn\">ĐĂNG NHẬP</a>\n"
                     + "                    <a href = \"signup\">ĐĂNG KÝ</a>\n"
                     + "                </div>\n");
-                    if(request.getAttribute("ms") != null) {
-                        out.println("<h2>" + request.getAttribute("ms") + "</h2>");
-                    }
-          out.println("                <div class =\"loginForm\">\n"
+            if (request.getAttribute("ms") != null) {
+                out.println("<h2>" + request.getAttribute("ms") + "</h2>");
+            }
+            out.println("                <div class =\"loginForm\">\n"
                     + "                    <form action = \"login\" method = \"post\">\n"
                     + "                            <label for = \"user\">Email hoặc số điện thoại</label>\n"
                     + "                            <br/><!-- comment -->\n"
-                    + "                            <input type =\"text\" id =\"user\" name =\"user\" placeholder =\"Email hoặc số điện thoại\"/>\n"
+                    + "                            <input type =\"text\" id =\"user\" name =\"user\" placeholder =\"Email hoặc số điện thoại\"  value = \"" + user + "\"/>\n"
                     + "\n"
                     + "                            <br/><!-- comment -->\n"
                     + "\n"
                     + "                            <label for = \"pass\">Mật khẩu</label>\n"
                     + "                            <br/><!-- comment -->\n"
-                    + "                            <input type =\"password\" id =\"pass\" name =\"pass\" placeholder =\"Mật khẩu\"/>\n"
+                    + "                            <input type =\"password\" id =\"pass\" name =\"pass\" placeholder =\"Mật khẩu\" value = \"" + pass + "\"/>\n"
                     + "\n"
-                    + "                            <br/>\n"
+                    + "                            <br/>\n");
+            out.println("<input type =\"checkbox\" id = \"rmbMe\" name =\"rem\" value =\"ON\"" + ((checked != null && checked.equals("ON"))?"checked":"") + "/>Remember me\n"
+                    + "            <!-- khi ng dung click vao checkbox ses co value la ON-->\n"
+                    + "            \n"
+                    + "            <br/><!-- comment -->"
                     + "\n"
                     + "                            <input id =\"logBtn\" type =\"submit\" value =\"ĐĂNG NHẬP\"/>\n"
                     + "                    </form>\n"
@@ -261,55 +286,55 @@ public class LoginServlet extends HttpServlet {
                     + "                <img src = \"images/logoCinema.png\">\n"
                     + "            </div>\n"
                     + "        </div>\n"
-                    + "        <div class = \"footer\">\n" +
-"            <div class = \"service\">\n" +
-"                <div class = \"cskh\">\n" +
-"                    <div class = \"tt\">\n" +
-"                        Chăm sóc khách hàng\n" +
-"                    </div>\n" +
-"                    <br/>\n" +
-"                    <div class = \"ct\">\n" +
-"                        <div class = \"d\">Hotline: 1900 6017</div>\n" +
-"\n" +
-"                        <div>Giờ làm việc: 8:00 - 22:00 (Tất cả các ngày bao gồm cả Lễ Tết)</div>\n" +
-"\n" +
-"                        <div>Email hỗ trợ: tranhaibang665@gmail.com</div>\n" +
-"                        <br/>\n" +
-"                    </div>\n" +
-"                </div>\n" +
-"\n" +
-"                <div class = \"lh\">\n" +
-"                    <div class = \"tt\">\n" +
-"                        Kết nối với chúng tôi\n" +
-"                    </div>\n" +
-"                    <br/>\n" +
-"                    <div class = \"ct\">\n" +
-"                        <a href = \"#\"><img src =\"images/facebookIcon.png\"/></a>\n" +
-"                        <a href = \"#\"><img src =\"images/twitterIcon.jpg\"/></a>\n" +
-"                        <a href = \"#\"><img src =\"images/instagramIcon.png\"/></a>\n" +
-"                        <a href = \"#\"><img src =\"images/zaloIcon.png\"/></a>\n" +
-"                    </div>\n" +
-"                </div>\n" +
-"            </div>\n" +
-"            \n" +
-"            <div class = \"tTin\">\n" +
-"                <div class = \"logoCty\">\n" +
-"                    <img src =\"images/logoCinema.png\"/>\n" +
-"                </div>\n" +
-"                \n" +
-"                <div class = \"abtCty\">\n" +
-"                    <span>CÔNG TY TNHH CJ MAST VIETNAM</span>\n" +
-"                    <br/>\n" +
-"                    Giấy CNĐKDN: 0303675393, đăng ký lần đầu ngày 31/7/2008, đăng ký thay đổi lần thứ 5 ngày 14/10/2015, cấp bởi thành phố Hà Nội.\n" +
-"                    <br/>\n" +
-"                    Địa Chỉ: ...\n" +
-"                    <br/>\n" +
-"                    Hotline: 1900 6017\n" +
-"                    <br/>\n" +
-"                    COPYRIGHT 2017 CJ MAST. All RIGHTS RESERVED\n" +
-"                </div>\n" +
-"            </div>\n" +
-"        </div>");
+                    + "        <div class = \"footer\">\n"
+                    + "            <div class = \"service\">\n"
+                    + "                <div class = \"cskh\">\n"
+                    + "                    <div class = \"tt\">\n"
+                    + "                        Chăm sóc khách hàng\n"
+                    + "                    </div>\n"
+                    + "                    <br/>\n"
+                    + "                    <div class = \"ct\">\n"
+                    + "                        <div class = \"d\">Hotline: 1900 6017</div>\n"
+                    + "\n"
+                    + "                        <div>Giờ làm việc: 8:00 - 22:00 (Tất cả các ngày bao gồm cả Lễ Tết)</div>\n"
+                    + "\n"
+                    + "                        <div>Email hỗ trợ: tranhaibang665@gmail.com</div>\n"
+                    + "                        <br/>\n"
+                    + "                    </div>\n"
+                    + "                </div>\n"
+                    + "\n"
+                    + "                <div class = \"lh\">\n"
+                    + "                    <div class = \"tt\">\n"
+                    + "                        Kết nối với chúng tôi\n"
+                    + "                    </div>\n"
+                    + "                    <br/>\n"
+                    + "                    <div class = \"ct\">\n"
+                    + "                        <a href = \"#\"><img src =\"images/facebookIcon.png\"/></a>\n"
+                    + "                        <a href = \"#\"><img src =\"images/twitterIcon.jpg\"/></a>\n"
+                    + "                        <a href = \"#\"><img src =\"images/instagramIcon.png\"/></a>\n"
+                    + "                        <a href = \"#\"><img src =\"images/zaloIcon.png\"/></a>\n"
+                    + "                    </div>\n"
+                    + "                </div>\n"
+                    + "            </div>\n"
+                    + "            \n"
+                    + "            <div class = \"tTin\">\n"
+                    + "                <div class = \"logoCty\">\n"
+                    + "                    <img src =\"images/logoCinema.png\"/>\n"
+                    + "                </div>\n"
+                    + "                \n"
+                    + "                <div class = \"abtCty\">\n"
+                    + "                    <span>CÔNG TY TNHH CJ MAST VIETNAM</span>\n"
+                    + "                    <br/>\n"
+                    + "                    Giấy CNĐKDN: 0303675393, đăng ký lần đầu ngày 31/7/2008, đăng ký thay đổi lần thứ 5 ngày 14/10/2015, cấp bởi thành phố Hà Nội.\n"
+                    + "                    <br/>\n"
+                    + "                    Địa Chỉ: ...\n"
+                    + "                    <br/>\n"
+                    + "                    Hotline: 1900 6017\n"
+                    + "                    <br/>\n"
+                    + "                    COPYRIGHT 2017 CJ MAST. All RIGHTS RESERVED\n"
+                    + "                </div>\n"
+                    + "            </div>\n"
+                    + "        </div>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -344,7 +369,26 @@ public class LoginServlet extends HttpServlet {
             throws ServletException, IOException {
         String u = request.getParameter("user");
         String p = request.getParameter("pass");
-        System.out.println(u + " " + p);
+        String r = request.getParameter("rem");
+
+        //tao 3 cookie: user, pass, remember me
+        Cookie user = new Cookie("user", u);
+        Cookie pass = new Cookie("pass", p);
+        Cookie rem = new Cookie("rem", r);
+
+        if (r != null) {
+            user.setMaxAge(60 * 60 * 24 * 7); //7 ngay
+            pass.setMaxAge(60 * 60 * 24 * 7); //7 ngay
+            rem.setMaxAge(60 * 60 * 24 * 7); //7 ngay
+        } else {
+            user.setMaxAge(0); //xoa
+            pass.setMaxAge(0); //xoa
+            rem.setMaxAge(0); //xoa
+        }
+        response.addCookie(user);
+        response.addCookie(pass);
+        response.addCookie(rem);
+
         AccountDAO acd = new AccountDAO();
         Account a = acd.check(u, p);
         if (a != null) {
